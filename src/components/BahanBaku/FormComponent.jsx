386face5729c7c32c938ce9e24fe829e52052df9
farararar <farah@investree.id>
2020-12-07 11:59:52 +0700

    add value to show last updated value

--- src/components/BahanBaku/FormComponent.jsx
+++ src/components/BahanBaku/FormComponent.jsx
@@ -2,23 +2,23 @@ import React, { useState, useEffect, useContext } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Select, MenuItem, InputLabel, FormControl, LinearProgress } from '@material-ui/core';
 import { MDBCard, MDBCardBody, MDBRow, MDBCol, MDBBtn, MDBBox } from 'mdbreact';
-import {Context as MaterialCategoryContext} from '../../services/Context/MaterialCategoryContext'
-import {Context as RawMaterialContext} from '../../services/Context/RawMaterialContext'
+import { Context as MaterialCategoryContext } from '../../services/Context/MaterialCategoryContext'
+import { Context as RawMaterialContext } from '../../services/Context/RawMaterialContext'
 import { useLocation } from "react-router-dom";
 
-const FormComponent = ({props}) => {
+const FormComponent = ({ props }) => {
     const location = useLocation();
 
     const classes = useStyles();
-    const {state:{listMaterialCategory}, ListMaterialCategory} = useContext(MaterialCategoryContext)
-    const {state, AddRawMaterial, ListUnit} = useContext(RawMaterialContext)
-    const params= {
+    const { state: { listMaterialCategory }, ListMaterialCategory } = useContext(MaterialCategoryContext)
+    const { state, AddRawMaterial, ListUnit } = useContext(RawMaterialContext)
+    const params = {
         raw_material_category_id: '',
-        name : '',
-        code : '',
-        type : '', 
-        unit_buy : '',
-        unit_use : '',
+        name: '',
+        code: '',
+        type: '',
+        unit_buy: '',
+        unit_use: '',
     }
     const [value, setValue] = useState(params);
     const [Update, setUpdate] = useState(true);
@@ -27,8 +27,8 @@ const FormComponent = ({props}) => {
         ListMaterialCategory();
         ListUnit();
         console.log('history = ', location);
-        setValue(location.state?location.state.data:params)
-        setUpdate(location.state?true:false);
+        setValue(location.state ? location.state.data : params)
+        setUpdate(location.state ? true : false);
     }, []);
 
     const handleChange = name => event => {
@@ -41,22 +41,22 @@ const FormComponent = ({props}) => {
     const handleReset = () => {
         setValue({
             raw_material_category_id: '',
-            name : '',
-            code : '',
-            type : '', 
-            unit_buy : '',
-            unit_use : '',
+            name: '',
+            code: '',
+            type: '',
+            unit_buy: '',
+            unit_use: '',
         })
     }
 
     const handleSave = () => {
         let data = {
             raw_material_category_id: value.raw_material_category_id,
-            name : value.name,
-            code : value.code,
-            type : value.type, 
-            unit_buy : value.unit_buy,
-            unit_use : value.unit_buy,
+            name: value.name,
+            code: value.code,
+            type: value.type,
+            unit_buy: value.unit_buy,
+            unit_use: value.unit_buy,
             unit_conversion: value.unit_conversion
         }
         AddRawMaterial(data, () => handleReset())
@@ -64,7 +64,7 @@ const FormComponent = ({props}) => {
 
     return (
         <div>
-            <h4>{!Update?'Tambah':'Edit'} Data Bahan Baku</h4>
+            <h4>{!Update ? 'Tambah' : 'Edit'} Data Bahan Baku</h4>
             <hr className="" />
             {/* {JSON.stringify(value)} */}
             {state.loading && (
@@ -75,15 +75,18 @@ const FormComponent = ({props}) => {
                     <MDBRow className='m-3'>
                         <MDBCol lg="7">
                             <TextField fullWidth label="Nama Bahan Baku" variant="outlined" margin="normal" value={value.name} onChange={handleChange('name')} />
-                            <TextField fullWidth label="Kode Bahan Baku" variant="outlined" margin="normal" value={value.code} onChange={handleChange('code')}/>
+                            <TextField fullWidth label="Kode Bahan Baku" variant="outlined" margin="normal" value={value.code} onChange={handleChange('code')} />
                             <FormControl fullWidth className={classes.formControl}>
                                 <InputLabel>Kategori Bahan Baku</InputLabel>
-                                <Select onChange={handleChange('raw_material_category_id')}>
+                                <Select
+                                    value={value.raw_material_category_id}
+                                    onChange={handleChange('raw_material_category_id')}>
                                     <MenuItem value="">
                                         <em>None</em>
                                     </MenuItem>
                                     {listMaterialCategory.map(item => (
-                                        <MenuItem value={item.id}>{item.name}</MenuItem>
+                                        <MenuItem value={item.id} selected>{item.name}
+                                        </MenuItem>
                                     ))}
                                 </Select>
                             </FormControl>
@@ -92,7 +95,9 @@ const FormComponent = ({props}) => {
                         <MDBCol lg="5">
                             <FormControl fullWidth className={classes.formControl}>
                                 <InputLabel>Jenis</InputLabel>
-                                <Select onChange={handleChange('type')}>
+                                <Select
+                                    value={value.type}
+                                    onChange={handleChange('type')}>
                                     <MenuItem value="">
                                         <em>None</em>
                                     </MenuItem>
@@ -102,22 +107,26 @@ const FormComponent = ({props}) => {
                             </FormControl>
                             <FormControl fullWidth className={classes.formControl}>
                                 <InputLabel>Satuan Beli</InputLabel>
-                                <Select onChange={handleChange('unit_buy')}>
+                                <Select
+                                    value={value.unit_buy}
+                                    onChange={handleChange('unit_buy')}>
                                     <MenuItem value="">
                                         <em>None</em>
                                     </MenuItem>
-                                    {state.listUnit&&state.listUnit.map((res)=>(
+                                    {state.listUnit && state.listUnit.map((res) => (
                                         <MenuItem value={res}>{res}</MenuItem>
                                     ))}
                                 </Select>
                             </FormControl>
                             <FormControl fullWidth className={classes.formControl}>
                                 <InputLabel>Satuan Guna</InputLabel>
-                                <Select onChange={handleChange('unit_use')}>
+                                <Select
+                                    value={value.unit_use}
+                                    onChange={handleChange('unit_use')}>
                                     <MenuItem value="">
                                         <em>None</em>
                                     </MenuItem>
-                                    {state.listUnit&&state.listUnit.map((res)=>(
+                                    {state.listUnit && state.listUnit.map((res) => (
                                         <MenuItem value={res}>{res}</MenuItem>
                                     ))}
                                 </Select>
